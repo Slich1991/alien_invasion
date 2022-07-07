cd7c081ddec500ee6676e2e405a1d1c34d4dae91
VSobolev <vova@unilimes.com>
2022-07-07 21:20:11 +0300

    07/07/2022 update

---
+++ game_stats.py
@@ -0,0 +1,15 @@
+class GameStats:
+    """Отслеживание статистики для игры Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Инициализирует статистику."""
+        self.score = None
+        self.ships_left = None
+        self.game_active = False
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Инициализирует статистику, изменяющуюся в ходе игры."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
--- settings.py
+++ settings.py
@@ -10,9 +10,41 @@ class Settings():
 
         # Настройки корабля
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Параметры снаряда
-        self.bullet_speed = 1
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # Настройки пришельцев
+        self.alien_points = None
+        self.alien_speed = 0.8
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+        self.ship_speed_factor = None
+        self.alien_speed_factor = None
+        self.bullet_speed_factor = None
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 1.0
+
+        self.fleet_direction = 1
+
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
